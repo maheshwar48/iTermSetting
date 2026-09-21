9ded289fb81a12e3c29209b86b5f6f63c8e445c9
Maheshwar Nepal <maheshwar.nepal@workday.com>
2026-09-21 16:22:26 -0700

    update

--- p10k.zsh
+++ p10k.zsh
@@ -31,7 +31,6 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    os_icon                 # os identifier (Unicode, not Nerd Font)
     dir                     # current directory
     vcs                     # git status
     command_execution_time  # duration of the last command (left = stays visible)
